8ba42e322da4c3a259b30d8978a4a326c03d4a40
Shuhei Matsuoka <matsuokashuheiii@gmail.com>
2022-06-27 15:54:19 +0000

    :construction:

---
+++ src/lib.rs
@@ -0,0 +1,48 @@
+use std::thread;
+
+pub struct ThreadPool {
+    // threads: Vec<thread::JoinHandle<()>>,
+    workers: Vec<Worker>,
+}
+
+impl ThreadPool {
+    /// Create a new TreadPool.
+    ///
+    /// The size is the number of threads in the pool.
+    ///
+    /// # Panics
+    ///
+    /// The `new` function will panic if the size is zero.
+    pub fn new(size: usize) -> ThreadPool {
+        // assert!(size > 0);
+        // let mut threads = Vec::with_capacity(size);
+        // for _ in 0..size {
+        //     // create some threads and store them in the vector
+        // }
+        // ThreadPool { threads }
+        assert!(size > 0);
+        let mut workers = Vec::with_capacity(size);
+        for id in 0..size {
+            workers.push(Worker::new(id));
+        }
+        ThreadPool { workers }
+    }
+
+    pub fn execute<F>(&self, f: F)
+    where
+        F: FnOnce() + Send + 'static,
+    {
+    }
+}
+
+struct Worker {
+    id: usize,
+    thread: thread::JoinHandle<()>,
+}
+
+impl Worker {
+    fn new(id: usize) -> Worker {
+        let thread = thread::spawn(|| {});
+        Worker { id, thread }
+    }
+}
